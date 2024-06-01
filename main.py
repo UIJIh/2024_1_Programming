45430a57740be29e12fe1b106e252d7eef85ad5c
Uiji <116754983+UIJIh@users.noreply.github.com>
2024-06-01 20:05:54 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-from utils.model_manager import load_senti_model, load_summ_model
+from utils.model_manager import load_senti_model, load_summ_model, load_groq_api, load_stable_diffusion
 from utils.diary_manager import load_diary
 from windows.gui import DiaryService
-# from diffusers import StableDiffusionPipeline
 import torch
 
 if __name__ == "__main__":
+    # 파일 준비
     diary = load_diary()
-    tokenizer, senti_model, config = load_senti_model()   
-    summarizer = load_summ_model()
-
-    if torch.cuda.is_available():
-        diffusion = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16)
-        diffusion = diffusion.to("cuda")
-    else: # gpu 안되면 이미지 생성 포기
-        diffusion = None
-        
-    app = DiaryService(diary, tokenizer, senti_model, config, summarizer, diffusion)
+    
+    # 모델 준비
+    tokenizer, senti_model, config = load_senti_model() # RoBerta
+    summarizer = load_summ_model() # t5
+    text_generator = load_groq_api() # llama
+    img_generator_base, img_generator_refiner = load_stable_diffusion() # gpu 사용 안되면 None 반환
+    
+    # gui와 함께 시스템 실행
+    app = DiaryService(diary, tokenizer, senti_model, config, 
+                       text_generator, summarizer, img_generator_base, img_generator_refiner)
     app.mainloop()
